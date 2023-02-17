42a220bdc4440b839021b9b5f933b6b8494f918b
ArtemisFowl0 <dupontmick872@gmail.com>
2023-02-17 22:08:55 +0100

    clean

--- data.tf
+++ data.tf
@@ -1,5 +1,6 @@
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
+/*
 data "aws_vpc" "default" {default = true}
 data "aws_security_groups" "default" {
   filter {
@@ -7,4 +8,4 @@ data "aws_security_groups" "default" {
     values = [data.aws_vpc.default.id]
   }
 }
-
+*/
